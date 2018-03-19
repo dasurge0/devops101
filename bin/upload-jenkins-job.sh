58a493afff1870ef195564206bf8f5ebcb3ac871
lxie123 <leexie@gmail.com>
2018-03-19 00:00:51 -0700

    fixed folder path

--- bin/upload-jenkins-job.sh
+++ bin/upload-jenkins-job.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp -r jenkins_jobs/* `kubectl -n spinnaker get pv -o jsonpath='{.items[?(@.spec.claimRef.name == "demo-jenkins")].spec.hostPath.path}'/jobs`
+cp -r ~/devops101/jenkins_jobs/* `kubectl -n spinnaker get pv -o jsonpath='{.items[?(@.spec.claimRef.name == "demo-jenkins")].spec.hostPath.path}'/jobs`
 
