96b9d4214e4b63412394f7230e9b3c2c728fa714
Lee <lxie@smashware.io>
2018-03-27 05:10:25 +0000

    fixed versions for install-k8

--- bin/install-k8s.sh
+++ bin/install-k8s.sh
@@ -2,10 +2,10 @@
 
 minikube_user="${SUDO_USER:-root}"
 
-curl -L -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -L -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.4/bin/linux/amd64/kubectl
 chmod +x /usr/local/bin/kubectl
 
-curl -Lo /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+curl -Lo /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases/v0.25.0/minikube-linux-amd64
 chmod +x /usr/local/bin/minikube
 
 minikube start --vm-driver none --memory 12000 --cpus 4 --bootstrapper localkube
