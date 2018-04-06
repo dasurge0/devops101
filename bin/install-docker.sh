a809490295fc882699b3bf1d6568858ab180da43
Lee Xie <lxie@smashware.io>
2018-04-06 22:05:09 +0000

    install-docker.sh edited online with Bitbucket

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -7,4 +7,4 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $
 apt-get update
 apt-get install -y  docker-ce=17.03.2~ce-0~ubuntu-xenial
 docker --version
-usermod -G docker ${SUDO_USER:-root}
+usermod -aG docker ${SUDO_USER:-root}
